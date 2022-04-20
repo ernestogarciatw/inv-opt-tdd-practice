84ed36df89b299302c0f51dbb77b14697a5607b8
test <test@tests-MacBook-Pro.local>
2022-04-20 06:56:22 -0500

    first commit

---
+++ src/main/kotlin/com/workrise/invoicingengine/tddpractice/TddPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.workrise.invoicingengine.tddpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TddPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TddPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/workrise/invoicingengine/tddpractice/TddPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.workrise.invoicingengine.tddpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TddPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
